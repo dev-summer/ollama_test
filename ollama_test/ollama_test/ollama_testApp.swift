5d1782218e5aef1f1afe03cc32fa047bc763da1f
Jiyoung Lee <lee.jiyoung@yanolja.com>
2024-09-30 11:30:30 +0900

    Create project

---
+++ ollama_test/ollama_test/ollama_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  ollama_testApp.swift
+//  ollama_test
+//
+//  Created by 지영C 이 on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ollama_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
